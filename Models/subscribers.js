4d16bc5601c5edec11e6f756f66aa66433c4c1c3
chandhan <chandhan.g0401@gmail.com>
2025-02-27 11:13:25 +0530

    completed required schema for admin dashboard

---
+++ Models/subscribers.js
@@ -0,0 +1,19 @@
+const mongoose=require("mongoose")
+
+const SubscribersSchema=mongoose.Schema={
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    }
+}
+
+const Subscribers=mongoose.model("Subscribers",SubscribersSchema)
+
+module.exports={
+    Subscribers
+
+}
